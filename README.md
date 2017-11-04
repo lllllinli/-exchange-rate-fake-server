3cc57f234e6cd088dc7bcc564bec932c8f797a78
lllllinli <standleylin@gmail.com>
2017-11-04 10:55:26 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # -exchange-rate-fake-server
- exchange-rate-fake-server
+ 
+ exchange rate fake server
+
+
+## 套件介紹
+
+* [ ] (opn)[https://www.npmjs.com/package/opn-cli]
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by apple on 2017/11/4.
+ */
+console.log('index.js');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log('Example app listening on port 3000!'));
